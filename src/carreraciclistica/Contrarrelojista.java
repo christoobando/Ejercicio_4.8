dfeef951f490c10459ccb5a4601e52dbab539864
Christopher <Christopher@DESKTOP-6E1TUBO>
2024-08-16 14:25:13 -0500

    Actividad 4 - Ejercicio 4

---
+++ src/carreraciclistica/Contrarrelojista.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package carreraciclistica;
+
+public class Contrarrelojista extends Ciclista {
+    private double velocidadMaxima;
+
+    public Contrarrelojista(int identificador, String nombre, double velocidadMaxima) {
+        super(identificador, nombre);
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    protected double getVelocidadMaxima() {
+        return velocidadMaxima;
+    }
+
+    protected void setVelocidadMaxima(double velocidadMaxima) {
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    protected void imprimir() {
+        super.imprimir();
+        System.out.println("Velocidad maxima = " + velocidadMaxima);
+    }
+
+    protected String imprimirTipo() {
+        return "Es un contrarrelojista";
+    }
+}
